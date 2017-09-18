2fd9951bb79af318597663ecbd1b00332501a4ea
Olof Markstedt <olof.markstedt@claremont.se>
2017-09-18 14:35:20 +0200

    Read from ByteBuffer. Removed U, V channels.

--- run.sh
+++ run.sh
@@ -12,8 +12,7 @@ rock -q +-Wall $ARGS $FLAGS $USE_FILE
 exitcode=$?
 if [[ $exitcode -eq 0 && -f ./Main ]]
 then
-	cat data | ./Main
-	exitcode=$?
+avconv -i ~/Video/UMPR\ Videos/HD\ Pole\ 1.MOV -f rawvideo -pix_fmt yuv420p -| ./Main |  avplay -f rawvideo -pix_fmt gray -s 1920x1080 -
 fi
 exit $exitcode
 
